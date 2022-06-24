7752ef6bedfae1ffcbc530b7a6d8926125b777ab
nksm <54118+nksm@users.noreply.github.com>
2022-06-24 16:15:40 +0900

    build multiple files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
+
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  root,
+  build: {
+    outDir,
+    rollupOptions: {
+      input: {
+        'a': resolve(root, 'styles', 'a.css'),
+        'b': resolve(root, 'styles', 'b.css'),
+        'index': resolve(root, 'index.html'),
+      },
+      output: {
+        assetFileNames: "[name].[ext]",
+      },
+    }
+  },
 })
